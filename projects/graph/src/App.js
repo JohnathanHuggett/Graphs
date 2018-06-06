134aa0cef7531de7dac11232d629b34bcd73450f
JohnathanHuggett <Johnathan.Huggett@gmail.com>
2018-06-06 15:57:53 -0400

    Johnathan Huggett, working on BFS

--- projects/graph/src/App.js
+++ projects/graph/src/App.js
@@ -3,8 +3,8 @@ import { Graph } from "./graph";
 import "./App.css";
 
 // !!! IMPLEMENT ME
-const canvasWidth = window.innerWidth;
-const canvasHeight = window.innerHeight;
+const canvasWidth = 750;
+const canvasHeight = 600;
 
 /**
  * GraphView
@@ -32,129 +32,36 @@ class GraphView extends Component {
         let ctx = canvas.getContext("2d");
 
         // Clear it
-        // boxes
-        // ctx.fillStyle = "black";
-        // ctx.fillRect(100, 100, 100, 100);
-        // ctx.fillRect(400, 100, 100, 100);
-        // ctx.fillRect(300, 300, 100, 100);
+        ctx.fillStyle = "blue";
+        ctx.fillRect(0, 0, canvasWidth, canvasHeight);
 
-        // draw many objects
+        ctx.textBaseline = "middle";
+        ctx.textAlign = "center";
+        ctx.font = "16px Arial";
 
-        // arcs
-        // for (let i = 0; i < 50; i++) {
-        //     const xSquare = Math.random() * canvasWidth;
-        //     const ySquare = Math.random() * canvasHeight;
+        const vertexSize = 15;
 
-        //     ctx.rect(xSquare, ySquare, 100, 100);
-        //     ctx.strokeStyle = "black";
-        //     ctx.stroke();
-        // }
-
-        // square
-        // for (let i = 0; i < 50; i++) {
-        //     const x = Math.random() * canvasWidth;
-        //     const y = Math.random() * canvasHeight;
-
-        //     ctx.beginPath();
-        //     ctx.arc(x, y, 30, 0, Math.PI * 2, false);
-        //     ctx.strokeStyle = "red";
-        //     ctx.stroke();
-        // }
-
-        // window resize
-        // window.addEventListener("resize", () => {
-        //     const canvasWidth = window.innerWidth;
-        //     const canvasHeight = window.innerHeight;
-        //     init();
-        // });
-
-        // mouse event
-        const mouse = {
-            x: null,
-            y: null,
-        };
-
-        window.addEventListener("mousemove", event => {
-            // console.log(event);
-            mouse.x = event.x;
-            mouse.y = event.y;
-            // console.log(mouse);
-        });
-
-        // circle creator below
-        function Circle(x, y, xV, yV, radius) {
-            this.x = x;
-            this.y = y;
-            this.xV = xV;
-            this.yV = yV;
-            this.radius = radius;
-            this.maxRadius = 80;
-            this.minRadius = radius;
-            this.colorArr = ["#CFC291", "#CFC291", "#A1E8D9", "#FF712C", "#695D46"];
-            this.color = this.colorArr[Math.floor(Math.random() * this.colorArr.length)];
-
-            this.draw = function() {
+        for (let vertex of this.props.graph.vertexes) {
+            for (let edge of vertex.edges) {
                 ctx.beginPath();
-                ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
-                ctx.fillStyle = this.color;
-                ctx.fill();
+                ctx.moveTo(vertex.pos.x, vertex.pos.y);
+                ctx.lineTo(edge.destination.pos.x, edge.destination.pos.y);
                 ctx.stroke();
-            };
-
-            this.update = function() {
-                if (this.x + this.radius > canvasWidth || this.x - this.radius < 0) {
-                    this.xV = -this.xV;
-                }
-
-                if (this.y + this.radius > canvasHeight || this.y - this.radius < 0) {
-                    this.yV = -this.yV;
-                }
-
-                this.x += this.xV;
-                this.y += this.yV;
-
-                // mouse interactivity
-                if (mouse.x - this.x < 50 && mouse.x - this.x > -50 && mouse.y - this.y < 50 && mouse.y - this.y > -50) {
-                    // controls max size
-                    if (this.radius < this.maxRadius) this.radius += 1;
-                }
-                // returns radius to original size
-                else if (this.radius > this.minRadius) this.radius -= 1;
-
-                this.draw();
-            };
-        }
-
-        const circleArr = [];
-
-        const circleNum = 1000;
-
-        for (let i = 0; i < circleNum; i++) {
-            let cirRadius = Math.random() * 3 + 1;
-            let x = Math.random() * (canvasWidth - cirRadius * 2) + cirRadius;
-            let y = Math.random() * (canvasHeight - cirRadius * 2) + cirRadius;
-            let xV = (Math.random() - 0.5) * 4;
-            let yV = (Math.random() - 0.5) * 4;
-            circleArr.push(new Circle(x, y, xV, yV, cirRadius));
-        }
-
-        // animation
-        function animate() {
-            requestAnimationFrame(animate);
-
-            ctx.clearRect(0, 0, canvasWidth, canvasHeight);
-
-            for (let i = 0; i < circleArr.length; i++) {
-                circleArr[i].update();
             }
         }
-        animate();
 
-        // !!! IMPLEMENT ME
-        // compute connected components
-        // draw edges
-        // draw verts
-        // draw vert values (labels)
+        for (let vertex of this.props.graph.vertexes) {
+            ctx.beginPath();
+            ctx.arc(vertex.pos.x, vertex.pos.y, vertexSize, 0, 2 * Math.PI);
+
+            ctx.fillStyle = "white";
+            ctx.fill();
+
+            ctx.fillStyle = "black";
+            ctx.fillText(vertex.value, vertex.pos.x, vertex.pos.y);
+
+            ctx.stroke();
+        }
     }
 
     /**
@@ -178,6 +85,10 @@ class App extends Component {
 
         // !!! IMPLEMENT ME
         // use the graph randomize() method
+        this.state.graph.randomize(5, 4, 150, 0.06);
+        this.state.graph.bfs();
+
+        // this.state.graph.debugCreateTestData();
     }
 
     render() {
--- projects/graph/src/graph.js
+++ projects/graph/src/graph.js
@@ -2,121 +2,159 @@
  * Edge
  */
 export class Edge {
-  // !!! IMPLEMENT ME
+    constructor(destination) {
+        this.destination = destination;
+        // implement weight
+    }
 }
 
 /**
  * Vertex
  */
 export class Vertex {
-  // !!! IMPLEMENT ME
+    constructor(value = "default", pos = { x: -1, y: -1 }) {
+        this.edges = [];
+        this.value = value;
+        this.pos = pos;
+    }
 }
 
 /**
  * Graph
  */
 export class Graph {
-  constructor() {
-    this.vertexes = [];
-  }
-
-  /**
-   * Create a random graph
-   */
-  randomize(width, height, pxBox, probability=0.6) {
-    // Helper function to set up two-way edges
-    function connectVerts(v0, v1) {
-      v0.edges.push(new Edge(v1));
-      v1.edges.push(new Edge(v0));
+    constructor() {
+        this.vertexes = [];
     }
 
-    let count = 0;
+    // debugCreateTestData() {
+    //     // console.log('called debugCreateTestData()');
+    //     let debugVertex1 = new Vertex("t1", { x: 40, y: 40 });
+    //     let debugVertex2 = new Vertex("t2", { x: 80, y: 80 });
+    //     let debugVertex3 = new Vertex("t3", { x: 40, y: 80 });
+    //     // debugVertex1.pos.x = 50;
+    //     // debugVertex1.pos.y = 60;
 
-    // Build a grid of verts
-    let grid = [];
-    for (let y = 0; y < height; y++) {
-      let row = [];
-      for (let x = 0; x < width; x++) {
-        let v = new Vertex();
-        //v.value = 'v' + x + ',' + y;
-        v.value = 'v' + count++;
-        row.push(v);
-      }
-      grid.push(row);
-    }
+    //     let debugEdge1 = new Edge(debugVertex2); // 1 to 2
+    //     let debugEdge2 = new Edge(debugVertex3); // 2 to 3
 
-    // Go through the grid randomly hooking up edges
-    for (let y = 0; y < height; y++) {
-      for (let x = 0; x < width; x++) {
-        // Connect down
-        if (y < height - 1) {
-          if (Math.random() < probability) {
-            connectVerts(grid[y][x], grid[y+1][x]);
-          }
+    //     this.vertexes.push(debugVertex1, debugVertex2, debugVertex3);
+
+    //     debugVertex1.edges.push(debugEdge1);
+    //     debugVertex2.edges.push(debugEdge2);
+    // }
+
+    /**
+     * Create a random graph
+     */
+    randomize(width, height, pxBox, probability = 0.6) {
+        // Helper function to set up two-way edges
+        function connectVerts(v0, v1) {
+            v0.edges.push(new Edge(v1));
+            v1.edges.push(new Edge(v0));
         }
 
-        // Connect right
-        if (x < width - 1) {
-          if (Math.random() < probability) {
-            connectVerts(grid[y][x], grid[y][x+1]);
-          }
+        let count = 0;
+
+        // Build a grid of verts
+        let grid = [];
+        for (let y = 0; y < height; y++) {
+            let row = [];
+            for (let x = 0; x < width; x++) {
+                let v = new Vertex();
+                //v.value = 'v' + x + ',' + y;
+                v.value = "v" + count++;
+                row.push(v);
+            }
+            grid.push(row);
         }
-      }
+
+        // Go through the grid randomly hooking up edges
+        for (let y = 0; y < height; y++) {
+            for (let x = 0; x < width; x++) {
+                // Connect down
+                if (y < height - 1) {
+                    if (Math.random() < probability) {
+                        connectVerts(grid[y][x], grid[y + 1][x]);
+                    }
+                }
+
+                // Connect right
+                if (x < width - 1) {
+                    if (Math.random() < probability) {
+                        connectVerts(grid[y][x], grid[y][x + 1]);
+                    }
+                }
+            }
+        }
+
+        // Last pass, set the x and y coordinates for drawing
+        const boxBuffer = 0.8;
+        const boxInner = pxBox * boxBuffer;
+        const boxInnerOffset = (pxBox - boxInner) / 2;
+
+        for (let y = 0; y < height; y++) {
+            for (let x = 0; x < width; x++) {
+                grid[y][x].pos = {
+                    x: (x * pxBox + boxInnerOffset + Math.random() * boxInner) | 0,
+                    y: (y * pxBox + boxInnerOffset + Math.random() * boxInner) | 0,
+                };
+            }
+        }
+
+        // Finally, add everything in our grid to the vertexes in this Graph
+        for (let y = 0; y < height; y++) {
+            for (let x = 0; x < width; x++) {
+                this.vertexes.push(grid[y][x]);
+            }
+        }
     }
 
-    // Last pass, set the x and y coordinates for drawing
-    const boxBuffer = 0.8;
-    const boxInner = pxBox * boxBuffer;
-    const boxInnerOffset = (pxBox - boxInner) / 2;
+    /**
+     * Dump graph data to the console
+     */
+    dump() {
+        let s;
 
-    for (let y = 0; y < height; y++) {
-      for (let x = 0; x < width; x++) {
-        grid[y][x].pos = {
-          'x': (x * pxBox + boxInnerOffset + Math.random() * boxInner) | 0,
-          'y': (y * pxBox + boxInnerOffset + Math.random() * boxInner) | 0
-        };
-      }
+        for (let v of this.vertexes) {
+            if (v.pos) {
+                s = v.value + " (" + v.pos.x + "," + v.pos.y + "):";
+            } else {
+                s = v.value + ":";
+            }
+
+            for (let e of v.edges) {
+                s += ` ${e.destination.value}`;
+            }
+            console.log(s);
+        }
     }
 
-    // Finally, add everything in our grid to the vertexes in this Graph
-    for (let y = 0; y < height; y++) {
-      for (let x = 0; x < width; x++) {
-        this.vertexes.push(grid[y][x]);
-      }
+    /**
+     * BFS
+     */
+    bfs(start) {
+        console.log("bfs called");
+        // !!! IMPLEMENT ME
+        // define color for vertexes
+        // take vertex (start) and add to found list
+        // while queue[0] && edge is not in found list
+        // add to found list
+        // boolean to determine if vertex is found?
+        // add to end of queue
+        // add color property to vertex
+        // dequeue queue[0]
+        // if queue is not empty, continue
     }
-  }
 
-  /**
-   * Dump graph data to the console
-   */
-  dump() {
-    let s;
-
-    for (let v of this.vertexes) {
-      if (v.pos) {
-        s = v.value + ' (' + v.pos.x + ',' + v.pos.y + '):';
-      } else {
-        s = v.value + ':';
-      }
-
-      for (let e of v.edges) {
-        s += ` ${e.destination.value}`;
-      }
-      console.log(s);
+    /**
+     * Get the connected components
+     */
+    getConnectedComponents() {
+        console.log("getConnectedComponents called");
+        // !!! IMPLEMENT ME
+        // go to next unfound vertex in graph.vertexes
+        // call bfs on it
+        // continue through length of arr
     }
-  }
-
-  /**
-   * BFS
-   */
-  bfs(start) {
-    // !!! IMPLEMENT ME
-  }
-
-  /**
-   * Get the connected components
-   */
-  getConnectedComponents() {
-    // !!! IMPLEMENT ME
-  }
 }
